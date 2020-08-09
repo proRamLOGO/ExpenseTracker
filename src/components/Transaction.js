9bb100ce24498f8b0a7a08c8b818fb466b62d6ca
proRamLOGO <kapilgupta547@outlook.com>
2020-08-09 19:14:49 +0530

    Del Working

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {GlobalContext} from '../context/GlobalState';
 
 export const Transaction = ({transaction}) => {
     const sign = transaction.amount<0 ? '-' : '+' ;
     const type = transaction.amount<0 ? "minus" : "plus" ;
+
+    const {deleteTransaction} = useContext(GlobalContext);
+
     return (  
       <li className={type} >
         {transaction.text}
         <strong><span>{sign} ₹{Math.abs(transaction.amount)}</span></strong>
-        <button className="delete-btn">x</button>
+        <button className="delete-btn" onClick={() => deleteTransaction(transaction.id)} >X</button>
       </li>
     )
 }
