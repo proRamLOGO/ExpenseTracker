9bec30f85a2a5c906bba38c8e08a88149401321c
proRamLOGO <kapilgupta547@outlook.com>
2020-08-09 23:41:34 +0530

    Updates

---
+++ precache-manifest.026ae0bdd8325950a1756b8f396373e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "883fbf7cf298cadd1f60c11f0b7b4d46",
+    "url": "/ExpenseTracker/index.html"
+  },
+  {
+    "revision": "bacbddd2d7d500f60337",
+    "url": "/ExpenseTracker/static/css/main.e6af8772.chunk.css"
+  },
+  {
+    "revision": "a3886eb48eeb822dea54",
+    "url": "/ExpenseTracker/static/js/2.abb637fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ExpenseTracker/static/js/2.abb637fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bacbddd2d7d500f60337",
+    "url": "/ExpenseTracker/static/js/main.4148cb5d.chunk.js"
+  },
+  {
+    "revision": "e25630aad13bae8f9aae",
+    "url": "/ExpenseTracker/static/js/runtime-main.0e810c6f.js"
+  }
+]);
